3de4967fc592693823f0af2b5369b778bd6e7f3a
Alex Ehlke <alex.ehlke@gmail.com>
2024-01-17 00:09:00 -0500

    wip

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.4
+// swift-tools-version: 5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -16,7 +16,7 @@ let package = Package(
 		.library(name: "DSFStepperView-shared", type: .dynamic, targets: ["DSFStepperView"]),
 	],
 	dependencies: [
-		.package(url: "https://github.com/dagronf/DSFAppearanceManager", from: "3.3.0"),
+        .package(url: "https://github.com/lake-of-fire/DSFAppearanceManager", branch: "main"),
 	],
 	targets: [
 		.target(name: "DSFStepperView", dependencies: ["DSFAppearanceManager"]),
